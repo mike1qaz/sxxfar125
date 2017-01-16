97c13c563651d33572b8375a6b9089a7e2c57b77
mikeye <zhanye115@qq.com>
2017-01-16 23:27:26 +0800

    0116

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -3,7 +3,6 @@
 <head>
 	<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-	<title>Sofar</title>
 	<title>首法</title>
     <meta name="description" content="sofar">
     <meta name="keywords" content="sofar">
